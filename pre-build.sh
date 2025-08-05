45a24e8b111ad8b1103efa54a4f15436f9a1775f
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-05 20:18:01 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-set -e                      # останавливаемся при любой ошибке
+set -e
 
-PATCH_DIR="${GITHUB_WORKSPACE}/patches"   # туда мы загрузили diff’ы
+PATCH_DIR="${GITHUB_WORKSPACE}/patches"
 
-# Переходим в исходники OpenVPN внутри Padavan
-cd padavan-ng/trunk/user/openvpn
+# !!! Переходим прямо в исходники OpenVPN
+cd padavan-ng/trunk/user/openvpn/openvpn-2.6.13
 
-# Применяем все diff'ы по порядку
 for p in "${PATCH_DIR}"/0*-tunnelblick-openvpn_xorpatch-*.diff; do
     echo ">>> Патчим $(basename "$p")"
+    # -p1   – срезаем "openvpn-2.6.x.old/"
+    # -t    – падаем, если хунк не применился
     patch -p1 -t < "$p"
 done
