0583f24e6dd9a3663ad7d8d7f51f25506170433f
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-05 11:50:04 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -3,7 +3,13 @@ set -e
 
 echo "=== Запуск pre-build.sh ==="
 
-echo "Смотрим, где padavan-ng:"
-ls -R | head -n 200
+SRC_DIR="padavan-ng/trunk/user/openvpn/openvpn-2.5.x"
 
-exit 0
+if [[ -d "$SRC_DIR" ]]; then
+    echo "Применяю XOR-патч в $SRC_DIR ..."
+    patch -d "$SRC_DIR" -p1 < patches/openvpn_xor.patch
+    echo "XOR-патч успешно применён!"
+else
+    echo "Ошибка: не найден каталог $SRC_DIR"
+    exit 1
+fi
