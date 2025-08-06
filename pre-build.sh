0084dce951e1bd3e1986d8e62d8cf42b8ae6d8dc
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-06 17:58:53 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -6,23 +6,24 @@ PATCH_URL="https://github.com/luzrain/openvpn-xorpatch/releases/download/v${OVPN
 
 for dir in padavan-ng/trunk/user/openvpn \
            padavan-ng/trunk/user/openvpn-openssl; do
-    mf="$dir/Makefile"            || continue
+    mf="$dir/Makefile"
     [[ -f $mf ]] || continue
-
     echo ">> patching $mf"
 
-    # 1. подменяем имя каталога и URL архива
+    # 1. архив и каталог
     sed -i "s|^SRC_NAME=.*|SRC_NAME=openvpn-${OVPN_VER}|" "$mf"
     sed -i "s|^SRC_URL=.*|SRC_URL=${PATCH_URL}|"          "$mf"
 
-    # 2. без дублирования добавляем --enable-xor-patch
-    grep -q -- '--enable-xor-patch' "$mf" || \
-      sed -i '/--enable-small/ s|$| \\\
-\t\t--enable-xor-patch|' "$mf"
+    # 2. удаляем ВСЕ старые вставки xor-patch и лишние обратные слэши
+    sed -i '/--enable-xor-patch/d' "$mf"
+    sed -i '/^[[:space:]]*\\[[:space:]]*$/d' "$mf"
 
-    # 3. на случай, если строка с openvpn-orig.patch жива — закомментируем
-    sed -i '/openvpn-orig\.patch/s|^[^#]|#&|' "$mf"
+    # 3. перезаписываем строку с --enable-small одной корректной версией
+    sed -i 's|--enable-small[[:space:]]*\\|--enable-small --enable-xor-patch \\|' "$mf"
+
+    # 4. комментируем возможный openvpn-orig.patch
+    sed -i '/openvpn-orig\.patch/ s|^[[:space:]]*|# &|' "$mf"
 done
 
-# 4. удаляем старый тар, чтобы стянулся свежий
+# 5. гарантируем повторную загрузку архива
 rm -f padavan-ng/trunk/dl/openvpn-${OVPN_VER}.tar.* || true
