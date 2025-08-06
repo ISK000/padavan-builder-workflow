fc3cf22a7d2404fa583672286f43fcb20463ef91
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-06 14:50:09 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -e
+
 OVPN_VER=2.6.14
-PATCHED_URL="https://github.com/luzrain/openvpn-xorpatch/releases/download/v${OVPN_VER}/openvpn-${OVPN_VER}.tar.gz"
+PATCH_URL="https://github.com/luzrain/openvpn-xorpatch/releases/download/v${OVPN_VER}/openvpn-${OVPN_VER}.tar.gz"
 
 for DIR in padavan-ng/trunk/user/openvpn \
-           padavan-ng/trunk/user/openvpn_ssl ; do
-    echo ">> Подменяем OpenVPN в $DIR"
-    curl -L -o "$DIR/openvpn-${OVPN_VER}.tar.gz" "$PATCHED_URL"
+           padavan-ng/trunk/user/openvpn-openssl   # убедитесь в названии!
+do
+    MF="$DIR/Makefile"    && [ -f "$MF" ] || continue
+    echo ">> Патчим $MF"
+    sed -i "s|^SRC_URL=.*|SRC_URL=${PATCH_URL}|" "$MF"
 done
+
+rm -f padavan-ng/trunk/dl/openvpn-${OVPN_VER}.tar.* || true
