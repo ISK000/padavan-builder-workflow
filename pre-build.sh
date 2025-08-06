f05d2f001c8f27f342713798ffa55ff62ab20d7c
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-06 17:29:10 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -4,19 +4,21 @@ set -e
 OVPN_VER=2.6.14
 PATCH_URL="https://github.com/luzrain/openvpn-xorpatch/releases/download/v${OVPN_VER}/openvpn-${OVPN_VER}.tar.gz"
 
-# 1. Переписываем URL у обеих сборок (openssl / uclibc)
 for dir in padavan-ng/trunk/user/openvpn \
-           padavan-ng/trunk/user/openvpn-openssl; do
-    mf="${dir}/Makefile"        || continue
-    [ -f "$mf" ] || continue
-    echo ">> Патчим $mf"
-    sed -i "s|^SRC_URL=.*|SRC_URL=${PATCH_URL}|" "$mf"
+           padavan-ng/trunk/user/openvpn-openssl
+do
+    mf="$dir/Makefile"         || continue
+    [[ -f $mf ]] || continue
+
+    echo ">> patching $mf"
+    # 1. подменяем архив и имя каталога
+    sed -i "s|^SRC_NAME=.*|SRC_NAME=openvpn-${OVPN_VER}|" "$mf"
+    sed -i "s|^SRC_URL=.*|SRC_URL=${PATCH_URL}|"          "$mf"
+
+    # 2. добавляем --enable-xor-patch (оставляем остальные опции автора)
+    sed -i '/--enable-small/ s|$| \\\
+\t\t--enable-xor-patch|' "$mf"
 done
 
-# 2. Удаляем старый архив, чтобы точно скачался новый
-rm -f padavan-ng/trunk/dl/openvpn-${OVPN_VER}.tar.* || true
-
-# 3. (необязательно) убираем строку, которая тащит openvpn-orig.patch,
-#    если она вдруг осталась раскомментированной
-sed -i '/openvpn-orig\.patch/s/^/#/' \
-  padavan-ng/trunk/user/openvpn*/Makefile || true
+# 3. оригинальный патч больше не нужен
+rm -f padavan-ng/trunk/user/openvpn*/openvpn-orig.patch || true
