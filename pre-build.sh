167a71967049f5ff944849ab4faf72d097a02aee
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-06 16:48:47 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -4,14 +4,12 @@ set -e
 OVPN_VER=2.6.14
 PATCH_URL="https://github.com/luzrain/openvpn-xorpatch/releases/download/v${OVPN_VER}/openvpn-${OVPN_VER}.tar.gz"
 
-for DIR in padavan-ng/trunk/user/openvpn \
-           padavan-ng/trunk/user/openvpn-openssl   # убедитесь в названии!
-do
-    MF="$DIR/Makefile"    && [ -f "$MF" ] || continue
+for DIR in padavan-ng/trunk/user/openvpn padavan-ng/trunk/user/openvpn-openssl; do
+    MF="$DIR/Makefile" ; [ -f "$MF" ] || continue
     echo ">> Патчим $MF"
-    sed -i "s|^SRC_URL=.*|SRC_URL=${PATCH_URL}|" "$MF"
+    sed -i \
+        -e "s|^[[:space:]]*SRC_NAME=.*|SRC_NAME=openvpn-${OVPN_VER}|" \
+        -e "s|^[[:space:]]*SRC_URL=.*|SRC_URL=${PATCH_URL}|"  "$MF"
 done
 
 rm -f padavan-ng/trunk/dl/openvpn-${OVPN_VER}.tar.* || true
-echo ">> wipe pre-built toolchain to match source timestamps"
-rm -rf padavan-ng/toolchain/out 2>/dev/null || true
