e4f129e5f7d34e9030c61d878000a8e6f4cd2a12
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-07 05:46:56 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,26 +2,31 @@
 set -e
 
 OVPN_VER=2.6.14
-BASE_URL="https://github.com/OpenVPN/openvpn/archive/refs/tags/v${OVPN_VER}.tar.gz"
+SRC_URL="https://github.com/OpenVPN/openvpn/archive/refs/tags/v${OVPN_VER}.tar.gz"
 PATCH_URL="https://raw.githubusercontent.com/luzrain/openvpn-xorpatch/master/patches/openvpn-${OVPN_VER}.patch"
 
 for dir in padavan-ng/trunk/user/openvpn \
            padavan-ng/trunk/user/openvpn-openssl; do
-    mf="${dir}/Makefile" || continue
+    mf="$dir/Makefile" || continue
     [[ -f $mf ]] || continue
 
+    echo "### правим $mf"
+
+    # 1. ставим правильный архив и имя каталога
     sed -i \
         -e "s|^SRC_NAME=.*|SRC_NAME=openvpn-${OVPN_VER}|" \
-        -e "s|^SRC_URL=.*|SRC_URL=${BASE_URL}|"          \
+        -e "s|^SRC_URL=.*|SRC_URL=${SRC_URL}|"           \
         "$mf"
 
-    # === добавляем накатывание патча =========================
-    sed -i "/tar -xf/s|$| \\\
-\t\t&& wget -qO- ${PATCH_URL} | patch -d \$(SRC_NAME) -p1|" "$mf"
+    # 2. выкачиваем xor-patch рядом с Makefile (один раз на сборку)
+    curl -sSL -o "$dir/openvpn-xor.patch" "${PATCH_URL}"
 
-    # подстрахуемся и раскомментируем возможную старую строку
-    sed -i '/openvpn-orig\.patch/s|^#||' "$mf"
+    # 3. раскомментируем строку патча и меняем её на новый файл
+    sed -i \
+        -e '/openvpn-orig\.patch/{
+                s|#\s*||; s|openvpn-orig\.patch|openvpn-xor.patch|
+            }' "$mf"
 done
 
-# старые архивы – в корзину
+# 4. старые исходники – удалить, чтобы кэш не мешал
 rm -f padavan-ng/trunk/dl/openvpn-${OVPN_VER}.tar.* 2>/dev/null || true
