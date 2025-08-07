a2513b2f0fe62321548a2ec11c1f917ba161526e
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-07 09:44:43 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,52 +1,44 @@
 #!/usr/bin/env bash
 set -e
 
-# Версия OpenVPN
 OVPN_VER=2.6.14
-# ‼️ Берём не «чистый» архив, а архив тега openvpn-xorpatch
 TAG_URL="https://github.com/luzrain/openvpn-xorpatch/archive/refs/tags/v${OVPN_VER}.tar.gz"
 
-# Папки с Makefile’ами Padavan’а, в которых правится исходник OpenVPN
 for dir in padavan-ng/trunk/user/openvpn \
            padavan-ng/trunk/user/openvpn-openssl; do
-
-    mf="$dir/Makefile"       || continue
+    mf="$dir/Makefile" || continue
     [[ -f $mf ]] || continue
     echo ">>> patching $mf"
 
-    # 1. Заменяем источник на наш архив тега
+    # 1. подставляем патчёный архив
     sed -i "s|^SRC_NAME=.*|SRC_NAME=openvpn-${OVPN_VER}|" "$mf"
     sed -i "s|^SRC_URL=.*|SRC_URL=${TAG_URL}|"            "$mf"
 
-    # 2. Удаляем флаг --enable-xor-patch (в патченых исходниках он НЕ нужен,
-    #     более того, configure на него ругается)
-    sed -i '/--enable-xor-patch/d' "$mf"
+    # 2. гарантируем наличие флага
+    grep -q -- '--enable-xor-patch' "$mf" || \
+      sed -i 's|--enable-small|--enable-small \\\n\t--enable-xor-patch|' "$mf"
 
-    # 3. Возвращаем autoreconf (нужен, потому что configure придётся пересобрать)
+    # 3. возвращаем autoreconf
     sed -i 's|^.*true # autoreconf disabled.*$|autoreconf -fi |' "$mf"
 
-    # 4. Игнорируем старый openvpn-orig.patch
+    # 4. отключаем старый openvpn-orig.patch
     sed -i '/openvpn-orig\.patch/s|^[^#]|#&|' "$mf"
 done
 
-# -----------------------------------------------------------------------------
-# 5. Формируем «правильный» tar-ball, который ожидает Padavan
-#    (в архиве тега папка называется openvpn-xorpatch-<ver>,
-#     Padavan ждёт openvpn-<ver>)
-# -----------------------------------------------------------------------------
-pushd padavan-ng/trunk/dl >/dev/null
 
-# чистим кеш, если вдруг был прежний файл
+# -----------------------------------------------------------------------------
+# Готовим tar-ball под ожидания Padavan’а
+# -----------------------------------------------------------------------------
+DL_DIR="padavan-ng/trunk/dl"
+mkdir -p "$DL_DIR"
+pushd "$DL_DIR" >/dev/null
+
 rm -f openvpn-${OVPN_VER}.tar.* tmp.tar.gz 2>/dev/null || true
 
-# качаем архив тега
 curl -L -o tmp.tar.gz "${TAG_URL}"
-
-# распаковываем, переименовываем директорию и упаковываем обратно
 tar -xf tmp.tar.gz
 mv openvpn-xorpatch-${OVPN_VER} openvpn-${OVPN_VER}
 tar -czf openvpn-${OVPN_VER}.tar.gz openvpn-${OVPN_VER}
-
-# уборка
 rm -rf openvpn-${OVPN_VER} tmp.tar.gz
-popd
+
+popd >/dev/null
