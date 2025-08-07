3a4107377b7c90839aa15d9a00ef367ae329f89d
ISK000 <86924849+ISK000@users.noreply.github.com>
2025-08-07 10:58:08 +0500

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,44 +1,39 @@
 #!/usr/bin/env bash
 set -e
 
+# ----------------------------------------------------------------------
+# версия + ссылка на готовый архив c XOR-patch
+# ----------------------------------------------------------------------
 OVPN_VER=2.6.14
-TAG_URL="https://github.com/luzrain/openvpn-xorpatch/archive/refs/tags/v${OVPN_VER}.tar.gz"
+RELEASE_URL="https://github.com/luzrain/openvpn-xorpatch/releases/download/v${OVPN_VER}/openvpn-${OVPN_VER}.tar.gz"
 
-for dir in padavan-ng/trunk/user/openvpn \
-           padavan-ng/trunk/user/openvpn-openssl; do
-    mf="$dir/Makefile" || continue
-    [[ -f $mf ]] || continue
-    echo ">>> patching $mf"
+# каталоги с пакетами OpenVPN у Padavan-NG
+OPENVPN_DIRS=(
+  "padavan-ng/trunk/user/openvpn"
+  "padavan-ng/trunk/user/openvpn-openssl"
+)
 
-    # 1. подставляем патчёный архив
-    sed -i "s|^SRC_NAME=.*|SRC_NAME=openvpn-${OVPN_VER}|" "$mf"
-    sed -i "s|^SRC_URL=.*|SRC_URL=${TAG_URL}|"            "$mf"
+for dir in "${OPENVPN_DIRS[@]}"; do
+    mf="${dir}/Makefile"          || continue
+    [[ -f $mf ]]                  || continue
+    echo ">>> refresh ${dir}"
 
-    # 2. гарантируем наличие флага
-    grep -q -- '--enable-xor-patch' "$mf" || \
-      sed -i 's|--enable-small|--enable-small \\\n\t--enable-xor-patch|' "$mf"
+    # 1. подчистка наследия
+    rm -rf "${dir}/openvpn-${OVPN_VER}" \
+           "${dir}/openvpn-${OVPN_VER}.tar."* 2>/dev/null || true
 
-    # 3. возвращаем autoreconf
-    sed -i 's|^.*true # autoreconf disabled.*$|autoreconf -fi |' "$mf"
+    # 2. скачиваем свежий архив
+    curl -L --retry 5 -o "${dir}/openvpn-${OVPN_VER}.tar.gz" "${RELEASE_URL}"
 
-    # 4. отключаем старый openvpn-orig.patch
-    sed -i '/openvpn-orig\.patch/s|^[^#]|#&|' "$mf"
+    # 3. патчим Makefile
+    sed -i "s|^SRC_NAME=.*|SRC_NAME=openvpn-${OVPN_VER}|"      "${mf}"
+    sed -i "s|^SRC_URL=.*|SRC_URL=${RELEASE_URL}|"             "${mf}"
+
+    grep -q -- '--enable-xor-patch' "${mf}" || \
+        sed -i 's/--enable-small/--enable-small \\\n\t--enable-xor-patch/' "${mf}"
+
+    sed -i 's|true # autoreconf disabled.*|autoreconf -fi |'    "${mf}"
+    sed -i '/openvpn-orig\.patch/s|^[^\t#]|#&|'                 "${mf}"
 done
 
-
-# -----------------------------------------------------------------------------
-# Готовим tar-ball под ожидания Padavan’а
-# -----------------------------------------------------------------------------
-DL_DIR="padavan-ng/trunk/dl"
-mkdir -p "$DL_DIR"
-pushd "$DL_DIR" >/dev/null
-
-rm -f openvpn-${OVPN_VER}.tar.* tmp.tar.gz 2>/dev/null || true
-
-curl -L -o tmp.tar.gz "${TAG_URL}"
-tar -xf tmp.tar.gz
-mv openvpn-xorpatch-${OVPN_VER} openvpn-${OVPN_VER}
-tar -czf openvpn-${OVPN_VER}.tar.gz openvpn-${OVPN_VER}
-rm -rf openvpn-${OVPN_VER} tmp.tar.gz
-
-popd >/dev/null
+echo ">>> prebuild.sh finished OK"
